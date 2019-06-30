6f727b9224224d3f7b19f42beb19f1ee44ce7c25
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 20:13:20 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 cart = {
   "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
-  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
-  "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
+  "KALE" => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK" => {:price => 4.50, :clearance => true,  :count => 1}
 }
 
 # cart.each do |item|
