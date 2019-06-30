3d7691c4403b4be36fb858a96fc59f75fd1ee033
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 20:14:36 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 cart = {
   "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
-  "KALE" => {:price => 3.00, :clearance => false, :count => 3}
+  "KALE" => {:price => 3.00, :clearance => false, :count => 3},
   "SOY MILK" => {:price => 4.50, :clearance => true,  :count => 1}
 }
 
