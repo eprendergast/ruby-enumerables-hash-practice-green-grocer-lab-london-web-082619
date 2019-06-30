ca3e80e8bbf320cc4db91577581fb16fa29a0846
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 20:35:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,10 +84,9 @@ end
 #input
 cart = {
   "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
-  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3},
   "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
 }
-
 
 #discount the price of every item on clearance by twenty percent
 def apply_clearance(cart)
