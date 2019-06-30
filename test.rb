2853f55c4d4782111bae48f55e04cbcf6b49e307
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 21:01:40 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -16,6 +16,65 @@ coupons = [
 		{:item => "CHEESE", :num => 3, :cost => 15.00}
 	]
 
+  def consolidate_cart(cart)
+    consolidated_cart = {}
+    cart.each do |item|
+      item.each do |key, value|
+        if consolidated_cart.has_key?(key) #if consolidated_cart already has that item...
+          consolidated_cart[key][:count] += 1 #increase the count by 1
+        else #else, if the item doesn't already exist in the consolidated_cart...
+          consolidated_cart[key] = value #add item to the cart
+          consolidated_cart[key][:count] = 1 #add a count to the attributes hash and set it equal to one
+        end
+      end
+    end
+    consolidated_cart
+  end
+
+  #given a consolidated cart, returns a cart with coupons applied
+  def apply_coupons(cart, coupons)
+    coupons.each do |cpn|
+      #set variable equal to the name of the discounted item for cleaner code
+      discounted_item = cpn[:item]
+      #if the item exists in the cart...
+      if cart[discounted_item] != nil
+        #if the cart has the minimum count of items for a coupon to be valid...
+        if cart[discounted_item][:count] >= cpn[:num]
+          #calculate the discounted price for the item
+          new_price = cpn[:cost] / cpn[:num]
+          #calculate the number of items remaining at full price
+          remaining_full_price = cart[discounted_item][:count] % cpn[:num] #3%2 = 1
+          #calculate the number of items that will have the discounted price applied
+          coupon_applied = cart[discounted_item][:count] - remaining_full_price
+          #maintain the clearance status
+          clearance = cart[discounted_item][:clearance]
+          #create new discounted item
+          cart["#{discounted_item} W/COUPON"] = {:price => new_price, :clearance => clearance, :count => coupon_applied}
+          #change the previous cart number to the number remaining
+          cart[discounted_item][:count] = remaining_full_price
+        #else, the cart doesn't have the minimum number of items required to qualify for the coupon
+        #OR the item doesn't exist in the cart
+        else
+          #donothing
+        end
+      end
+    end
+    cart
+  end
+
+  #discount the price of every item on clearance by twenty percent
+  def apply_clearance(cart)
+    cart.each do |item, attributes|
+      if attributes[:clearance] == true
+        clearance_price = attributes[:price] * 0.8
+        attributes[:price] = clearance_price.round(2)
+      else
+        #do nothing
+      end
+    end
+    cart
+  end
+
 #Create a checkout method that calculates the total cost of the consolidated cart
 #It consolidates the cart, applies coupons, and applies discounts. Then, it totals the cost of the entire cart,
 #accounting for each item and their prices, and returns this value.
