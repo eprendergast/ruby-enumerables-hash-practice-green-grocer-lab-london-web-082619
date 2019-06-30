8552bf18a4f8e9294d5e39e84ce7f659a649f84c
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 20:34:11 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -9,15 +9,15 @@ cart.each do |item, attributes|
 end
 
 
-# #discount the price of every item on clearance by twenty percent
-# def apply_clearance(cart)
-#   cart.each do |item, attributes|
-#     if item[:clearance] == true
-#       item[:price] *= 0.2
-#       (item[:price]).round(2)
-#     else
-#       #do nothing
-#     end
-#   end
-#   cart
-# end
+#discount the price of every item on clearance by twenty percent
+def apply_clearance(cart)
+  cart.each do |item, attributes|
+    if attributes[:clearance] == true
+      attributes[:price] *= 0.2
+      (attributes[:price]).round(2)
+    else
+      #do nothing
+    end
+  end
+  cart
+end
