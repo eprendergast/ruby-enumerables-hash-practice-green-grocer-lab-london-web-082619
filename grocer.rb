5bb90da60aebbf1cbd5796cde304f0a91547dea6
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-30 20:09:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -81,9 +81,31 @@ end
 # }
 
 
+#input 
+cart = {
+  "PEANUT BUTTER" => {:price => 3.00, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 4.50, :clearance => true,  :count => 1}
+}
+#discount the price of every item on clearance by twenty percent
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, attributes| 
+    if item[:clearance] == true
+      item[:price] *= 0.2 
+      item[:price].round(2)
+    else
+      #do nothing
+    end
+  end
+  cart
 end
+
+#output
+{
+  "PEANUT BUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
+}
 
 def checkout(cart, coupons)
   # code here
